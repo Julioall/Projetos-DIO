83fb12d502ac191850dd4d426fd230b5e0317602
julioall <julio.all@outlook.com>
2024-11-11 08:08:05 -0300

    Cria sistema de agendamento de taregas com mvc

---
+++ C#/SistemaAgendamentoDeTarefa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaAgendamentoDeTarefa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
